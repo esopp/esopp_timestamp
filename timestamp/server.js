2d2957cb5300a457d3bf130da1d7bcdeafe1f519
emily <esoppenheim@gmail.com>
2016-07-17 16:15:20 +0000

    first working version of the timestamp api.

--- timestamp/server.js
+++ timestamp/server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
+var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 app.get('/:DATE', function (req, res) {
-  var date = req.params.DATE.split('%20')
-  res.send(date);
+  var date = new Date((req.params.DATE.split(" ").length == 3 ? req.params.DATE : parseInt(req.params.DATE)*1000))
+  var fmt = {};
+  if (date.toString()  != 'Invalid Date') 
+  fmt = { 'unix' : date.getTime()/1000, 'natural' : month[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear() }
+  else fmt = {'unix' : null, 'natural' : null}
+  res.end(JSON.stringify(fmt));
+  
 });
 
 app.listen(8080, function () {
